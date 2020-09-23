960bb08c17de0886ec81a86613dd62480f8a202a
Rana-Siroosian <rana.siroosian@gmail.com>
2020-09-23 14:48:59 -0400

    Almost done with xml files

---
+++ app/src/main/java/com/example/android/dogs/view/ListFragment.kt
@@ -0,0 +1,29 @@
+package com.example.android.dogs.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.android.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
+
+
+class ListFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+
+
+
+}
